85911e27371deffdd88aa12698fac880eac31c2f
Maciej Rachuna <rachuna.maciej@gmail.com>
2025-07-07 13:56:17 +0200

    feat!: DEVOPS-201 optymalizacja modułu

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "gitlab_project" "project" {
   name                        = var.name
   description                 = var.description
+  archived                    = var.archived
   namespace_id                = data.gitlab_group.parent.id
   initialize_with_readme      = true
   default_branch              = var.default_branch == "" ? null : var.default_branch
@@ -9,10 +10,10 @@ resource "gitlab_project" "project" {
   visibility_level            = var.visibility
   build_git_strategy          = var.build_git_strategy
   autoclose_referenced_issues = var.autoclose_referenced_issues
-  avatar                      = var.icon_type != "" ? "${path.module}/images/${var.icon_type}.png" : (local.allowed_project_types[var.project_type].icon_type != "" ? "${path.module}/images/${local.allowed_project_types[var.project_type].icon_type}.png" : null)
-  avatar_hash                 = var.icon_type != "" ? filesha256("${path.module}/images/${var.icon_type}.png") : (local.allowed_project_types[var.project_type].icon_type != "" ? filesha256("${path.module}/images/${local.allowed_project_types[var.project_type].icon_type}.png") : null)
+  avatar                      = local.avatar == null ? null : "${local.avatar}"
+  avatar_hash                 = local.avatar == null ? null : filesha256("${local.avatar}")
 
-  archived = var.archived
+
 
   lifecycle {
     prevent_destroy = true
@@ -21,10 +22,10 @@ resource "gitlab_project" "project" {
 }
 
 resource "gitlab_project_push_rules" "push_rule" {
-  count   = var.is_enable_conventional_commits_push_rule == true ? 1 : 0
-  project = gitlab_project.project.id
+  count = var.is_gitlab_free == true ? 0 : 1
 
-  commit_message_regex = "^(build|chore|ci|docs|params|feat|fix|perf|refactor|style|test|revert|merge|release|hotfix|fixup|squash|wip|BREAKING CHANGE)(\\(.+\\))?: .+"
+  project              = gitlab_project.project.id
+  commit_message_regex = var.is_enable_conventional_commits_push_rule == true ? "^(build|chore|ci|docs|params|feat|fix|perf|refactor|style|test|revert|merge|release|hotfix|fixup|squash|wip|BREAKING CHANGE)(\\(.+\\))?: .+" : ""
 }
 
 resource "gitlab_project_label" "label" {
